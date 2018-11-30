0a17590d20bc59b8cac90f994d28bbb6b6a17b3d
Juozas Masiulis <compositum@gmail.com>
2018-11-30 09:49:54 +0200

    Stop using jellyfish

--- find.py
+++ find.py
@@ -1,22 +1,56 @@
 import multiprocessing as mp
 import re
 from collections import ChainMap
+from difflib import SequenceMatcher
 from typing import Dict, TextIO
 
 import click
-import jellyfish
 
 
-def find_matches(line: str, keyword: str, n: int) -> Dict[str, int]:
+def soundex(input: str) -> str:
+    '''returns soundex code of input string'''
+    letter_map = {'b': 1, 'f': 1, 'p': 1, 'v': 1,
+                  'c': 2, 'g': 2, 'j': 2, 'k': 2, 'q': 2, 's': 2, 'x': 2, 'z': 2,
+                  'd': 3, 't': 3, 'l': 4, 'm': 5, 'n': 5, 'r': 6}
+    input = input.lower()
+    exclude = 'eyuioahw'
+    result = []
+    result.append(input[0])
+    previous = letter_map.get(input[0], None)
+    count = 1
+    previous_vowel = False
+    for letter in input[1:]:
+        if letter not in exclude:
+            letter_code = letter_map[letter]
+            if (letter_code != previous) or (letter_code == previous and previous_vowel):
+                result.append(letter_code)
+                previous = letter_code
+                count += 1
+                previous_vowel = False
+            if count == 4:
+                break
+        elif letter not in 'hw':
+            previous_vowel = letter
+    output = "".join(map(str, result)).upper()
+    output = output+'0'*(4-len(output)) # pad to 4 letters
+    return output
+
+
+def _find_distance(str1: str, str2: str) -> float:
+    if str1 == str2:
+        return 0
+    matcher = SequenceMatcher(isjunk=None, a=str1.upper(), b=str2.upper())
+    return 1 - matcher.ratio()
+
+
+def find_matches(line: str, keyword: str, n: int) -> Dict[str, float]:
     '''Finds the best n matches in a given line'''
     words: str = re.sub(r'[^a-zA-Z]+', ' ', line)  # replace everything that does not look like a character with a space
-    similarity: Dict[str, int] = {}
+    similarity: Dict[str, float] = {}
     for word in set(words.split()):
-        similarity[word] = jellyfish.levenshtein_distance(
-            jellyfish.soundex(keyword),
-            jellyfish.soundex(word))
+        similarity[word] = _find_distance(soundex(keyword), soundex(word))
     results = sorted(similarity, key=similarity.get)
-    best_results: Dict[str, int] = {}
+    best_results: Dict[str, float] = {}
     for result in results[:n]:
         best_results[result] = similarity[result]
     return best_results
@@ -36,7 +70,7 @@ def main(file: TextIO, keyword: str, n: int) -> None:
 
     cpu_count: int = mp.cpu_count()
     pool = mp.Pool(cpu_count)
-    best_results: Dict[str, int] = {}
+    best_results: Dict[str, float] = {}
     while True:
         lines = [file.readline() for _ in range(cpu_count)]  # read a line for each cpu core
         if not any(lines):
--- test_find.py
+++ test_find.py
@@ -1,15 +1,27 @@
 from click.testing import CliRunner
 
-from find import find_matches, main
+from find import find_matches, main, soundex
+
+
+def test_soundex():
+    assert soundex('xxxx') == 'X000'
+    assert soundex('lituania') == 'L350'
+    assert soundex('robert') == soundex('rupert')
+    assert soundex('Rubin') == 'R150'
+    assert soundex('ashcraft') == soundex('ashcroft')
+    assert soundex('Tymczak') == 'T522'
+    assert soundex('Pfister') == 'P236'
+    assert soundex('Honeyman') == 'H555'
 
 
 def test_find_matches():
     line = 'Lithuania (UK and US: Listeni/ˌlɪθuːˈeɪniə/,[11][12][13] Lithuanian: Lietuva'
     keyword = 'lituania'
-    n = 5
+    n = 6
     find_matches_result = find_matches(line, keyword, n)
-    assert len(find_matches_result) == 5
-    assert find_matches_result == {'Lietuva': 1, 'Listeni': 2, 'Lithuania': 0, 'Lithuanian': 1, 'l': 2}
+    assert len(find_matches_result) == 6
+    assert find_matches_result == {'Lietuva': 0.25, 'Listeni': 0.25, 'Lithuania': 0, 'Lithuanian': 0.25, 'l': 0.5,
+                                   'and': 0.5}
 
 
 def test_cli():
